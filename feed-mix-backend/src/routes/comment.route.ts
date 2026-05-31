04624c3e65a4cbb131c5076969621e44f2780313
devwilson <ohioleomole@gmail.com>
2026-05-31 13:34:43 +0000

    update comment route

--- feed-mix-backend/src/routes/comment.route.ts
+++ feed-mix-backend/src/routes/comment.route.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
-import { getComments } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getComments,
+} from "../controllers/comment.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router: Router = express.Router();
 
 router.get("/post/:postId", getComments);
+
+router.post("/post/:postId", protectRoute, createComment);
 
 export default router;
