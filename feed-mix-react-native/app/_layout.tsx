055b68cbe064a5c098e4083a22a4f08ad3752c14
devwilson <ohioleomole@gmail.com>
2026-05-31 18:59:54 +0000

    implement authentication with clerk

--- feed-mix-react-native/app/_layout.tsx
+++ feed-mix-react-native/app/_layout.tsx
@@ -1,11 +1,13 @@
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+
 import "../global.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
@@ -14,19 +16,23 @@ export const unstable_settings = {
   anchor: "(tabs)",
 };
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Stack>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+        {/* <StatusBar style="auto" /> */}
+      </ClerkProvider>
     </ThemeProvider>
   );
 }
